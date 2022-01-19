ad8207fa063d72e1ec4550e23fee683de29cb82a
Sahil Bansal <61084646+sahilbansal0@users.noreply.github.com>
2022-01-19 13:48:21 +0530

    quiz app

---
+++ questions.js
@@ -0,0 +1,94 @@
+let questions = [{
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language",
+        options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Which of the following is not JavaScript Data Types?",
+        answer: "Float",
+
+        options: [
+            "Undefined",
+            "Number",
+            "Boolean",
+            "Float"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Which company developed JavaScript? ",
+        answer: "Netscape",
+        options: [
+            "Netscape",
+            "Bell Labs",
+            "Sun Microsystems",
+            "IBM"
+
+        ]
+    },
+
+    {
+        numb: 8,
+        question: " What are the types of Pop up boxes available in JavaScript?",
+        answer: "All of the above",
+        options: [
+            "Alert",
+            "Prompt",
+            "Confirm",
+            "All of the above"
+        ]
+
+    },
+];
